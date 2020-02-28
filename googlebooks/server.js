20eef70d2fcf2bb235858483cbcd02c63ae73b44
Monica Amitin <monzielvsu@gmail.com>
2020-02-28 10:42:33 -0800

    setup server.js

--- googlebooks/server.js
+++ googlebooks/server.js
@@ -1,5 +1,23 @@
 const express = require("express");
-const path = require("path");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require("./routes");
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("clinet/build");)
+}
+
+app.use(routes);
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+        {useCreateIndex: true, useNewUrlParser: true}
+);
+
+app.listen(PORT, () =>
+console.log(`?? ==> API server now on port ${PORT}!`)
+);
