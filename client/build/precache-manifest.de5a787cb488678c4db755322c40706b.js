77ff8601cba93d7df83562c57cb8890bd1920717
Monica Amitin <monzielvsu@gmail.com>
2020-03-11 21:42:05 -0700

    npm fix

---
+++ client/build/precache-manifest.de5a787cb488678c4db755322c40706b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7cef287ed509eda3f2a",
+    "url": "/static/js/main.4336f61f.chunk.js"
+  },
+  {
+    "revision": "8541336674e454d39220",
+    "url": "/static/js/2.299eba4c.chunk.js"
+  },
+  {
+    "revision": "6b99dc1e46ef19c0ed757a82fd62a100",
+    "url": "/index.html"
+  }
+];
